c7bb2cabf20bcb21753a5c7d3e0cf17e9950c2ef
Pedro <pedrotoneti150@gmail.com>
2024-02-13 12:03:31 -0300

    postman

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const express = require('express');
+const express = require("express");
 const server = express();
 
-server.get('/usuario', (req,res) => {
-    return res.json({usuario: 'Pedro'})
+server.get("/usuario", (req, res) => {
+  console.log("pedro");
+
+  return res.json({ usuario: "Pedro" });
+});
+server.delete("/delete", (req, res) => {
+  return res.json({ usuario: "Pedro" });
+});
+server.delete("/delete", (req, res) => {
+  return res.json({ usuario: "Pedro" });
 });
 
 server.listen(3000, () => {
- console.log('Servidor esta funcionando')
+  console.log("Servidor esta funcionando");
 });
